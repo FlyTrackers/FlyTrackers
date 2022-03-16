858ec6c304bc8fdd4b7943a6b29716d2534b01d7
Elliott Larsen <larsenel@oregonstate.edu>
2022-03-15 21:04:55 -0500

    A new Xcode project.

---
+++ FlyTrackers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlyTrackers
+//
+//  Created by Elliott Larsen on 3/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
