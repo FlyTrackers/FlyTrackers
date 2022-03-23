80e09dad7ee3412a4633db7c62e174ca42534483
Giovanni Propersi <giovannigp@gmail.com>
2022-03-22 22:39:52 -0500

    Added Login and Register functions to Parse functions.

--- FlyTrackers/API/Parse_API.swift
+++ FlyTrackers/API/Parse_API.swift
@@ -10,20 +10,59 @@ import Parse
 
 class ParseAPICaller {
     
-    // Register a New User
-    func registerNewUser(username: String, password: String) {
-        let new_user = PFUser()
-        new_user.username = username
-        new_user.password = password
-        
-        new_user.signUpInBackground{ (success, error) in
-            switch error {
-                case .some(let error as NSError):
-                    print(error.localizedDescription)
-                
-                case .none:
-                    print("Success")
+    // MARK:  - Register New User
+    
+    func registerNewUser(username: String, password: String, completion: @escaping (Result<Bool, NSError>) -> ()) {
+        let newUser = PFUser()
+        newUser.username = username
+        newUser.password = password
+
+        newUser.signUpInBackground { (succeeded, error) in
+            if let error = error {
+                completion(.failure(error as NSError))
+            } else {
+                completion(.success(succeeded))
             }
         }
     }
+    
+    /// Example parse call for registering new user
+    ///     let parseAPI = ParseAPICaller()
+    ///     parseAPI.registerNewUser(username: "Bobo", password: "Bob", completion: { result in
+    ///         switch result {
+    ///             case .success(let registered):
+    ///                 print("Registered: \(registered)")
+    ///             case .failure(let error):
+    ///                 // Handle Error
+    ///                 print(error.localizedDescription)
+    ///        }
+    ///    })
+    
+    // MARK: - Login New User
+    
+    func loginUser(username: String, password: String, completion: @escaping (Result<PFUser?, NSError>) -> ()) {
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                completion(.failure(error as NSError))
+            } else {
+                completion(.success(user))
+            }
+        }
+    }
+    
+    /// Example parse call for logging in user
+    ///     let parseAPI = ParseAPICaller()
+    ///     parseAPI.loginUser(username: "Bob", password: "Bob", completion: { result in
+    ///         switch result {
+    ///             case .success(let loggedIn):
+    ///                 print("Logged in: \(String(describing: loggedIn!.username!))")
+    ///                 print("Current user is:")
+    ///                 print(PFUser.current()!.username!)
+    ///             case .failure(let error):
+    ///                 // Handle error
+    ///                 print(error.localizedDescription)
+    ///         }
+    ///     })
+
+    
 }
--- FlyTrackers/LoginViewController.swift
+++ FlyTrackers/LoginViewController.swift
@@ -15,13 +15,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let parseAPI = ParseAPICaller()
-        
-        parseAPI.registerNewUser(username: "Bob", password: "Bob")
-        
-        
-        //ParseAPICaller.registerNewUser(username: "Bob", password: "Bob")
-        
+                
         // Do any additional setup after loading the view.
     }
     
