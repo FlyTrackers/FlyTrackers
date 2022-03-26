32d93e4d09f5c836d08b665710e56770c92b936a
Christian Franklin <christian.m.franklin@gmail.com>
2022-03-26 11:16:38 -0400

    Created login functionality, segues using Register and Guest buttons. Login button with error logic.

--- FlyTrackers/LoginViewController.swift
+++ FlyTrackers/LoginViewController.swift
@@ -12,18 +12,22 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var loginUsernameField: UITextField!
     @IBOutlet weak var loginPasswordField: UITextField!
+    @IBOutlet weak var loginErrorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let parseAPI = ParseAPICaller()
+        //let parseAPI = ParseAPICaller()
         
-        parseAPI.registerNewUser(username: "Bob", password: "Bob")
+        //parseAPI.registerNewUser(username: "Bob", password: "Bob")
         
         
         //ParseAPICaller.registerNewUser(username: "Bob", password: "Bob")
         
         // Do any additional setup after loading the view.
     }
+    
+    
+    
     
 
     /*
@@ -40,6 +44,28 @@ class LoginViewController: UIViewController {
     /*
      Login button.  Authentication and retrieval of user info.
      */
+        let username = loginUsernameField.text!
+        let password = loginPasswordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password)
+        { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                //print("Error: \(error?.localizedDescription)")
+                self.loginErrorLabel.text = "Invalid username/password."
+                
+            }
+        }
+        
+    }
+   
+    @IBAction func newRegister(_ sender: Any) {
+        self.performSegue(withIdentifier: "registerSegue", sender: nil)
+    }
+    
+    @IBAction func loginGuest(_ sender: Any) {
+        self.performSegue(withIdentifier: "loginSegue", sender: nil)
     }
     
 }
